c77dde16968fd9627fda4d58d500bb8692924606
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2020-11-16 08:40:45 +0000

    css html "complete",  first draft of js

---
+++ app.js
@@ -0,0 +1,160 @@
+//variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// this is our main cart!
+let cart = [];
+// these are our buttons
+let buttonsDOM = [];
+
+//getting the products
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      //   below is rebuilding the objects in to a usable way, using the map method
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products; // this is our newly built object
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+//display prodcuts
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach((product) => {
+      result += `
+        <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>£${product.price}</h4>
+        </article>
+        
+        `;
+    });
+    productsDOM.innerHTML = result;
+  }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      let inCart = cart.find((item) => item.id === id);
+      if (inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      } else {
+        button.addEventListener("click", (event) => {
+          event.target.innerText = "In Cart";
+          event.target.disabled = true;
+          //get product from prodcuts
+          let cartItem = { ...Storage.getProduct(id), amount: 1 }; //just turned this in to an object & gave it an amount proprty so that cart can add it up
+          //add prodcut to the cart
+          cart = [...cart, cartItem]; //the empty array
+          //save cart in local storage
+          Storage.saveCart(cart);
+          // set cart values
+          this.setCartValues(cart);
+          //display cart item
+          this.addCartItem(cartItem);
+          //show the cart
+          this.showCart()
+        });
+      }
+    });
+  }
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map((item) => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2)); //decimals for money
+    cartItems.innerText = itemsTotal;
+    console.log(cartTotal, cartItems);
+  }
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = ` <img src=${item.image} alt="product" />
+  <div>
+    <h4>${item.title}</h4>
+    <h5>£${item.price}</h5>
+    <span class="remove-item" data-id=${item.id}>remove</span>
+  </div>
+  <div>
+    <i class="fas fa-chevron-up" data-id=${item.id}></i>
+    <p class="item-amount">${item.amount}</p>
+    <i class="fas fa-chevron-down" data-id=${item.id}></i>
+  </div>`;
+    cartContent.appendChild(div);
+  }
+  showCart() {
+    cartOverlay.classList.add("transparentBcg");
+    cartDOM.classList.add("showCart");
+  }
+  setupAPP(){}
+}
+
+//local storage
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find((product) => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  static getCart(){
+      return localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+//setup application
+ui.setupAPP();
+  //get all products
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
+});
